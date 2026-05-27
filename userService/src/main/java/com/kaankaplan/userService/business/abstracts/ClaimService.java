24f18d6f9c7fff1b650720ca38b49f66f712e394
Sutharsan879 <sutharsanmurugan87@gmail.com>
2026-05-27 16:17:25 +0530

    base commit

--- userService/src/main/java/com/kaankaplan/userService/business/abstracts/ClaimService.java
+++
@@ -1,8 +0,0 @@
-package com.kaankaplan.userService.business.abstracts;
-
-import com.kaankaplan.userService.entity.Claim;
-
-public interface ClaimService {
-
-    Claim getClaimByClaimName(String claimName);
-}
